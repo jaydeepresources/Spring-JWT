822c130f69f1837539bd66db466c1e2b77a899ec
jaydeep apte <jaydeepresources@gmail.com>
2024-07-30 18:13:38 +0530

    Update WebSecurityConfig.java

--- spring-security-jwt/src/main/java/com/jwt/WebSecurityConfig.java
+++ spring-security-jwt/src/main/java/com/jwt/WebSecurityConfig.java
@@ -42,7 +42,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest()
+		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate/**").permitAll().anyRequest()
 				.authenticated().and().exceptionHandling().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
