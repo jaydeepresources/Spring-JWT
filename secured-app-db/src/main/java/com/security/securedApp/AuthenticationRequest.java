0b0e3f1ba63211cf9922ba1872399785ad4eac74
jaydeep apte <jaydeepresources@gmail.com>
2024-09-05 21:37:52 +0530

    Add files via upload

---
+++ secured-app-db/src/main/java/com/security/securedApp/AuthenticationRequest.java
@@ -0,0 +1,43 @@
+package com.security.securedApp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+
+    public AuthenticationRequest() {
+    }
+
+    public AuthenticationRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthenticationRequest{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ secured-app-db/src/test/java/com/security/securedApp/SecuredAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.securedApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
